9a498fd1b9a02fa0602b8e5c48b3114acd03ff3e
figueroar <raul.a.figueroa@hotmail.com>
2019-12-18 21:25:28 -0500

    Done.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,8 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
+
+  def ingredient_recipe=(recipe)
+    self.recipe = Recipe.find_or_create_by(title: recipe[:title])
+    self.recipe.update
+  end 
 end
